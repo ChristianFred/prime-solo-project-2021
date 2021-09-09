7e135d43be063011489bceabbd0665740a3d1c1f
Christian Fredricks <Christianpfred@gmail.com>
2021-09-08 21:17:55 -0500

    Added Database Tables to database.sql

--- database.sql
+++ database.sql
@@ -6,5 +6,32 @@
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "password" VARCHAR (1000) NOT NULL,
+    "email" VARCHAR (500) UNIQUE NOT NULL
+);
+
+CREATE TABLE "Matchup" (
+    "id" SERIAL PRIMARY KEY,
+    "outcome" BOOLEAN,
+    "myCharacter" INTEGER, 
+    "enemyCharacter" INTEGER,
+    "user_id" INT REFERENCES "user" NOT NULL
+);
+
+CREATE TABLE "Characters" (
+    "id" SERIAL PRIMARY KEY,
+    "Character" VARCHAR (250), 
+    "image_path" VARCHAR (250)
+);
+
+CREATE TABLE "MatchupItems" (
+    "id" SERIAL PRIMARY KEY,
+    "winner_Id" INT REFERENCES "Matchup" NOT NULL,
+    "items_Id" INT REFERENCES "Items" NOT NULL
+); 
+
+CREATE TABLE "Items" (
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR (250) NOT NULL, 
+    "image_path" VARCHAR (250)
 );
