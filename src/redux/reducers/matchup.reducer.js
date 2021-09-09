a11fa50adf7430cc163507d9dc8c3a6ac87a3d38
Christian Fredricks <Christianpfred@gmail.com>
2021-09-09 09:23:20 -0500

    Added some Reducer work

---
+++ src/redux/reducers/matchup.reducer.js
@@ -0,0 +1,10 @@
+const matchupReducer = (state = [], action) => {
+    switch(action.type) {
+        case 'SET_MATCHUP_LIST' :
+            return action.payload;
+        default: 
+        return state;
+    }
+}
+
+export default matchupReducer;
